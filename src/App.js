c9f350cf9c9e9b3322d7e5101b5233282993cf27
Gaspy <mpgasbarro@gmail.com>
2020-10-31 16:14:37 -0400

    Was able to get the Quiz Components Contents to only populate on the page

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
-import Home from "./Components/Home/Home"
-import Header from "./Components/Header/Header"
-import Modal from "./Components/Modal/Modal"
-import Quiz from "./Components/Quiz/Quiz"
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Modal from './Components/Modal/Modal';
+import Quiz from './Components/Quiz/Quiz';
 import './App.css';
-
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			questions: this.props.questions,
 			show: false,
-			
 		};
 	}
 	// Function that opens/closes Modal
 	showModal = () => {
-		this.setState({ show: !this.state.show })
-	}
-
-
+		this.setState({ show: !this.state.show });
+	};
 	render() {
 		return (
+			<div>
+				<header>
+					<Header />
+					{/* Input button for Modal */}
+					<input
+						className='open-modal-btn'
+						type='button'
+						onClick={this.showModal}
+						value=' Show Modal'
+					/>
+					<Modal show={this.state.show} onClose={this.showModal}>
+						This message is from Modal
+					</Modal>
+				</header>
+
 				<div>
-					<header>
-						<Header />
-						{/* Input button for Modal */}
-						<input
-							className='open-modal-btn'
-							type='button'
-							onClick={this.showModal}
-							value=' Show Modal'
-						/>
-
-						<Modal show={this.state.show} onClose={this.showModal}>
-							This message is from Modal
-						</Modal>
-					</header>
-					<Home />
-
-					<div>
-						<Switch>
-							<Route exact path='/quiz'>
-								<Quiz />
-							</Route>
-						</Switch>
-					</div>
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/quiz' component={Quiz} />
+					</Switch>
 				</div>
-
+			</div>
 		);
 	}
 }
-
 export default App;
