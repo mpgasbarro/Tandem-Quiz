22ead9a0cb49ab1fc707425702cfb2c69f68436e
Gaspy <mpgasbarro@gmail.com>
2020-11-01 11:09:04 -0500

    have merged correct answers with incorrect answers into an array and have them randomizing on the webpage properly.

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 
+let answerArr = [];
+let randomNum = Math.floor(Math.random() * 4);
+let correctAns = '';
+let allAns = '';
 
 class Quiz extends Component {
-    render() {
-        let questionInfo = this.props.questions.map((question, index) => {
-						return (
-								<li className='pecList' key={index}>
-									{question.question}
-								</li>
-						);
-				});
-        console.log(this.props.questions[0].question)
-        return (
-					<div>
-						{this.props.location.state.username}
-						<h1> {questionInfo} </h1>
-					</div>
-				);
-    }
+	render() {
+		let questionInfo = this.props.questions.map((question, index) => {
+			return <li key={index}>{question.question}</li>;
+		});
+
+		let answerInfo = this.props.questions.map((answer, index) => {
+			correctAns = answer.correct;
+			answerArr = answer.incorrect;
+			answerArr.splice(randomNum, 0, correctAns);
+			let allAns = answerArr.map((answers, index) => {
+				return <li> {answers} </li>;
+			});
+			return <h3 key={index}>{allAns}</h3>;
+		});
+
+		return (
+			<div>
+				{this.props.location.state.username}
+				<h1> {questionInfo[1]} </h1>
+				<h3> {answerInfo[1]} </h3>
+			</div>
+		);
+	}
 }
 
 export default Quiz;
